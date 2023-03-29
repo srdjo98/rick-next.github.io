13b8ad38455eb74c6ca59b1ee9c4bab46d041591
srdjo98 <zaricsrdjan10@gmail.com>
2023-03-29 13:07:21 +0200

    refactor

--- utils/index.tsx
+++ utils/index.tsx
@@ -9,6 +9,6 @@ export const status: FilterProps[] = [
 ];
 
 export const genders: FilterProps[] = [
-  { value: "female", label: "Female" },
-  { value: 'male', label: 'Male' },
+  { value: 'female', label: 'Female' },
+  { value: "male", label: "Male" },
 ];
