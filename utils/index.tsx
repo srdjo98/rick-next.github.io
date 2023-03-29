6c23402a4545b6b42fdb92216287fb7b9158eaff
srdjo98 <zaricsrdjan10@gmail.com>
2023-03-29 13:35:12 +0200

    test

--- utils/index.tsx
+++ utils/index.tsx
@@ -7,7 +7,7 @@ export const status: FilterProps[] = [
   { value: 'alive', label: 'Alive' },
   { value: 'dead', label: 'Dead' },
 ];
-
+console.log('asdasd')
 export const genders: FilterProps[] = [
   { value: 'female', label: 'Female' },
   { value: "male", label: "Male" },
